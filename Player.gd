55ad7d43bdaafc0450490d1efb824cf6f99ea83e
R9_ <81550154+Razboinicul@users.noreply.github.com>
2023-06-13 15:15:44 +0300

    Player(not tested)

--- Player.gd
+++ Player.gd
@@ -1,5 +1,10 @@
 extends KinematicBody
+export var SPEED = 10
 
 func _process(delta):
 	var velocity = Vector3.ZERO
-	
+	if Input.is_action_pressed("forward"): velocity += transform.basis.z * SPEED
+	if Input.is_action_pressed("backward"): velocity -= transform.basis.z * SPEED
+	if Input.is_action_pressed("right"): velocity += transform.basis.x * SPEED
+	if Input.is_action_pressed("left"): velocity -= transform.basis.x * SPEED
+	move_and_slide(velocity, Vector3.UP)
