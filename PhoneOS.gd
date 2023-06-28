c0463757a64ccbbecb53d5757d2ab3d01a41143d
R9_ <neamtrares@naver.com>
2023-06-28 14:14:00 +0300

    Added Shutdown button to PhoneOS

--- PhoneOS.gd
+++ PhoneOS.gd
@@ -1,4 +1,3 @@
 extends Control
-
-func _on_Button_pressed():
-	get_tree().change_scene("res://PhoneApp.tscn")
+func _on_Button_pressed(): get_tree().change_scene("res://PhoneApp.tscn")
+func _on_Shutdown_pressed(): get_tree().change_scene("res://TestScene3D.tscn")
