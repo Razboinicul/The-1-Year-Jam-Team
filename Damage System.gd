dac0cb6619cc093aa7330262998c4dec7f760f2a
R9_ <neamtrares@naver.com>
2023-06-30 21:45:26 +0300

    Damage System Update

--- Damage System.gd
+++ Damage System.gd
@@ -1,4 +1,5 @@
-extends Node2D
+extends Control
+signal on_damage_calculated(damage)
 export var damage = 0
 var div = 1.7
 var run = true
@@ -7,7 +8,7 @@ var body_list = {}
 func _process(delta):
 	$"Line Detection".position = $ColorRect.rect_position
 	if run:
-		if $ColorRect.rect_position.x >= 550: run = false
+		if $ColorRect.rect_position.x >= 600: run = false
 		elif Input.is_key_pressed(KEY_SPACE): run = false
 		else: $ColorRect.rect_position.x += 10
 	else:
@@ -15,8 +16,8 @@ func _process(delta):
 			var dmg = i.replace(" ", ".")
 			div = float(dmg)
 		damage = int($ColorRect.rect_position.x/div)
-		print("Dmg: ", damage)
-		print("div: ", div)
+		emit_signal("on_damage_calculated", damage)
+		queue_free()
 
 func _on_Line_Detection_body_entered(body): body_list[body.name] = body
 func _on_Line_Detection_body_exited(body): body_list.erase(body.name)
--- TestScene.gd
+++ TestScene.gd
@@ -1,8 +1,14 @@
 extends Control
 
 var dialouge_preload = preload("res://Dialogue.tscn")
+var ds_preload = preload("res://Damage System.tscn")
 var run = 0
-func _next_dialouge(): print('dialouge done!!')
+func _get_damage(dmg): print("Damage: ", dmg)
+func _next_dialouge(): 
+	print('dialouge done!!')
+	var ds = ds_preload.instance()
+	ds.connect("on_damage_calculated", self, "_get_damage") 
+	add_child(ds)
 
 func _process(delta):
 	if run == 0:
